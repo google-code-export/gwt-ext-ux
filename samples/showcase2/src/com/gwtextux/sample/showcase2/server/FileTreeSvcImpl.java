fa7e1b3d5c945065e1df062ce385c09f0ec97631
mlim1972 <mlim1972@c29af4ec-dd45-0410-b099-ff09075b3556>
2008-05-25 07:00:54 +0000

    Removing extra import

--- samples/showcase2/src/com/gwtextux/sample/showcase2/server/FileTreeSvcImpl.java
+++ samples/showcase2/src/com/gwtextux/sample/showcase2/server/FileTreeSvcImpl.java
@@ -11,7 +11,6 @@ import java.util.Vector;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
