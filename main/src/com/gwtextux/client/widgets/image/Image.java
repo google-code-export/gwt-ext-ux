8dea2ea97aba34e9cd8044de93a952d93cdc12d5
mlim1972 <mlim1972@c29af4ec-dd45-0410-b099-ff09075b3556>
2008-06-30 02:08:42 +0000

    Issue 28 in UX.  For some reason the old code was not working.  It is changed to update the Element itself.

--- main/src/com/gwtextux/client/widgets/image/Image.java
+++ main/src/com/gwtextux/client/widgets/image/Image.java
@@ -3,10 +3,13 @@ package com.gwtextux.client.widgets.image;
 import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.Element;
+import com.gwtext.client.core.DomHelper;
 import com.gwtext.client.core.EventCallback;
 import com.gwtext.client.core.EventManager;
 import com.gwtext.client.core.EventObject;
+import com.gwtext.client.core.Function;
 import com.gwtext.client.core.ListenerConfig;
+import com.gwtext.client.util.JavaScriptObjectHelper;
 import com.gwtext.client.widgets.BoxComponent;
 import com.gwtext.client.widgets.menu.Menu;
 
@@ -19,6 +22,7 @@ public class Image extends BoxComponent {
 	private Element img = null;
 	private ListenerConfig listenerConfig = new ListenerConfig();
 	private Menu menu = null;
+	private JavaScriptObject imageArray = null;
     
 	/**
 	 * Constructor to create the Image Widget.
@@ -26,14 +30,18 @@ public class Image extends BoxComponent {
 	 * @param imgURL The URL for the image
 	 */
     public Image(String id, String imgURL) {
-
-        super(DOM.createImg());
-        img = this.getElement();
+    	super();
+        img = DOM.createImg();
+        this.setEl(img);
         
         DOM.setElementProperty(img, "src", imgURL);
         DOM.setElementProperty(img, "id", id);
         
+        imageArray = JavaScriptObjectHelper.createJavaScriptArray();
+        addImage(id, imgURL);
+        
         listenerConfig.setPreventDefault(true);
+        getOrCreateJsObj();
      }
 
     /**
@@ -125,17 +133,59 @@ public class Image extends BoxComponent {
     	EventManager.addListener(getElement(), "mouseover", onMouseOver, listenerConfig);  
     	EventManager.addListener(getElement(), "contextmenu", onMouseDown, listenerConfig);
     }
+    //  the method below did not work...
+	//  public native void addListener(ImageListener listener) /*-{
+	//  this.@com.gwtext.client.widgets.BoxComponent::addListener(Lcom/gwtext/client/widgets/event/BoxComponentListener;)(listener);
+	//  var componentJ = this;
+	//
+	//  this.@com.gwtext.client.widgets.Component::addListener(Ljava/lang/String;Lcom/google/gwt/core/client/JavaScriptObject;)('click',
+	//          function(self, event) {
+	//              var e = @com.gwtext.client.core.EventObject::instance(Lcom/google/gwt/core/client/JavaScriptObject;)(event);
+	//              listener.@com.onlineassets.client.widgets.ImageListener::onClick(Lcom/onlineassets/client/widgets/Image;Lcom/gwtext/client/core/EventObject;)(componentJ, e);
+	//          }
+	//  );
+	//}-*/;
+    
+    // TODO: make the add image loads the images separately so the can be used 
+    // later to change the image on different events or time based...
+    public void addImage(String id, String src){
+        JavaScriptObject element = JavaScriptObjectHelper.createObject();
+        JavaScriptObjectHelper.setAttribute(element, "id", id);
+        JavaScriptObjectHelper.setAttribute(element, "src", src);
+        JavaScriptObjectHelper.setArrayValue(imageArray, 
+        		JavaScriptObjectHelper.arrayLength(imageArray), element);
+    }
+    
+    /**
+     * This method resizes the image based on the max width provided.
+     * The method will adjust the height keeping the ratio.
+     * @param maxWidth the max width for the image
+     */
+    public void setMaxWidthKeepRatio(int maxWidth){
+    	float w= Integer.parseInt(DOM.getElementProperty(img, "width"));
+    	float h= Integer.parseInt(DOM.getElementProperty(img, "height"));
+		if (w > maxWidth) {
+		  float f=1f-((w - maxWidth) / w);
+		  DOM.setElementProperty(img, "width", Float.toString(w*f));
+		  DOM.setElementProperty(img, "height", Float.toString(h*f));
+		}
+    }
+    
+    /**
+     * This method resizes the image based on the max height provided.
+     * The method will adjust the width keeping the ratio.
+     * @param maxHeight the max height for the image
+     */
+    public void setMaxHeightKeepRatio(int maxHeight){
+    	float w= Integer.parseInt(DOM.getElementProperty(img, "width"));
+    	float h= Integer.parseInt(DOM.getElementProperty(img, "height"));
+		if (h > maxHeight) {
+		  float f=1f-((h - maxHeight) / h);
+		  DOM.setElementProperty(img, "width", Float.toString(w*f));
+		  DOM.setElementProperty(img, "height", Float.toString(h*f));
+		}
+    }
+    
 
-//    public native void addListener(ImageListener listener) /*-{
-//        this.@com.gwtext.client.widgets.BoxComponent::addListener(Lcom/gwtext/client/widgets/event/BoxComponentListener;)(listener);
-//        var componentJ = this;
-//
-//        this.@com.gwtext.client.widgets.Component::addListener(Ljava/lang/String;Lcom/google/gwt/core/client/JavaScriptObject;)('click',
-//                function(self, event) {
-//                    var e = @com.gwtext.client.core.EventObject::instance(Lcom/google/gwt/core/client/JavaScriptObject;)(event);
-//                    listener.@com.onlineassets.client.widgets.ImageListener::onClick(Lcom/onlineassets/client/widgets/Image;Lcom/gwtext/client/core/EventObject;)(componentJ, e);
-//                }
-//        );
-//    }-*/;
    
 }
